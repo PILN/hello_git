60eb66718090e4d74b4604af00e4c98b668db4a7
PILN <matveys909@gmail.com>
2025-06-28 12:04:50 +0300

    Пенис

---
+++ script.py
@@ -0,0 +1,9 @@
+class Yaraloh():
+    def __init__(self, gomosex):
+        if gomosex == 'Да':
+            print('Яра гей')
+        else:
+            print('Все равно гей')
+
+
+yara = Yaraloh('Да')
